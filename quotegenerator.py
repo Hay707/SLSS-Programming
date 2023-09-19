a16a473bae7d26338208ca47964854834706860e
Hay707 <hayleychan1430@gmail.com>
2023-09-19 10:53:48 -0700

    Added some new notes and Python files

---
+++ quotegenerator.py
@@ -0,0 +1,18 @@
+# Quote Generator
+# Author: Hayley
+# Date: September 19 2023
+
+# Output a quote from someone famous
+print("RAH! -- Anonymous")
+
+# Print out another quote
+print("asdnfiqwnelifqu")
+
+# Print out another, different quote
+print("... -- silence")
+
+# Ask the user a question
+input("What was your favourite quote?")
+ 
+# Respond with something a human would say
+print("Oh that's my favourite too!")
