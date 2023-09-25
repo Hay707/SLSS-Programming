d427d365766a2471d42dbbf45e70e762a7dc9436
Hay707 <hayleychan1430@gmail.com>
2023-09-25 12:31:56 -0700

    Added chatbot

--- chatbot.py
+++ chatbot.py
@@ -1,4 +1,19 @@
 # Chatbot
 # Author: Hayley
-# Date September 21 2023
+# Date September 25 2023
+
+# Greet the user
+print("Hello there!")
+print("I'm a crude chatbot, here to talk to you.")
+
+# Get the user's name and store in a variable
+user_name = input("What's your name?")
+
+# Respond with the users name
+print(f"It's nice to meet you, {user_name}!")
+
+# Ask the user what their favourite food is
+
+
+# Respond with something appropriate
 
