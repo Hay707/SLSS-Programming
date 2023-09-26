f567d429213377e58743b41d0dbb9465be8aebba
Hay707 <hayleychan1430@gmail.com>
2023-09-26 11:12:20 -0700

    Added time.sleep

--- chatbot.py
+++ chatbot.py
@@ -2,12 +2,20 @@
 # Author: Hayley
 # Date September 25 2023
 
+# Put imports under header
+import random
+import time 
+
 # Greet the user
+# Pause in between lines of dialogue
 print("Hello there!")
+time.sleep(1.5)
 print("I'm a crude chatbot, here to talk to you.")
+time.sleep(1.5)
 
 # Get the user's name and store in a variable
 user_name = input("What's your name?")
+time.sleep(1)
 
 # Respond with the users name
 print(f"It's nice to meet you, {user_name}!")
@@ -18,13 +26,15 @@ fave_food = input("What's your favourite food?")
 # Respond with something that is NOT TOO repetitive
 # Create a list of appropriate responses
 list_of_fave_food_responses = [
-    "Mmmmmm. Tha sounds delicious.",
+    "Mmmmmm. That sounds delicious.",
     f"Wa!{fave_food} sounds good.",
     "RAH!!!",
     "Cool!"
 ]
+
 # Choose one response randomly frmo the list
-import random
+
 random_response = random.choice(list_of_fave_food_responses)
 # Print out the random response
 print(random_response)
+
