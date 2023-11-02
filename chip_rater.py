b633ccbb5e0e043a186c578b1b8fdaa6ad0d9416
Hay707 <hayleychan1430@gmail.com>
2023-11-02 14:00:02 -0700

    Added chip rater

---
+++ chip_rater.py
@@ -0,0 +1,27 @@
+# Chip Rater
+# Author: Hayley
+# November 2 2023
+
+# Interview people about their perception of 
+# the taste of potato chips. We will ask them
+# a set of questions. In the end, we'll give an
+# aggregate score. 
+
+# Greet the user (being frienly as possible lol)
+print("Please answer the following questions on the chip you just ate. :)")
+
+# Create a list of questions to ask
+questions = [
+    "How crispy is the chip on a scale on 1 to 5? 5 is very crispy, 1 is mushy"
+    "How would you rate the taste from 1 to 5? 5 is best chip ever. 1 is I'd rather not eat it"
+    "From 1 to 5, how would you rate the packaging. 1 is someone got paid to put this together?" 
+]
+
+# For every question in that list: 
+    # Ask the question
+    # Get the user's rating
+    # Add the rating to some total-rating 
+
+
+# Do some math on the results
+# Present the results
