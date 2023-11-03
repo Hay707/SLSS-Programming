25e829411b80b0d70f1b90d336e99c38d2ab63e6
Hay707 <hayleychan1430@gmail.com>
2023-11-03 12:11:45 -0700

    Forgot to push

--- chip_rater.py
+++ chip_rater.py
@@ -12,16 +12,25 @@ print("Please answer the following questions on the chip you just ate. :)")
 
 # Create a list of questions to ask
 questions = [
-    "How crispy is the chip on a scale on 1 to 5? 5 is very crispy, 1 is mushy"
-    "How would you rate the taste from 1 to 5? 5 is best chip ever. 1 is I'd rather not eat it"
+    "How crispy is the chip on a scale on 1 to 5? 5 is very crispy, 1 is mushy",
+    "How would you rate the taste from 1 to 5? 5 is best chip ever. 1 is I'd rather not eat it",
     "From 1 to 5, how would you rate the packaging. 1 is someone got paid to put this together?" 
 ]
 
-# For every question in that list: 
-    # Ask the question
-    # Get the user's rating
-    # Add the rating to some total-rating 
+total_rating = 0
 
+# For every question in that list: 
+for question in questions:               # singular question
+    # Get the user's rating
+    user_rating = int(input(question + "\n").strip(".,?!"))           # converting string into number, \n = users answer will be on the next line
+    
+    # Add the rating to some total-rating 
+    total_rating += user_rating 
 
 # Do some math on the results
+# Use the average score to represent the chip's rating
+    average_rating = total_rating/ len(questions)        # len = length | so length of question
+
 # Present the results
+print(f'The average rating for this chip is: {average_rating:.2f}/ 5')     # put two decimal places for the float (:.2f)
+
