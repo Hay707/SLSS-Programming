466a9b58c9f3fed526655b6e301d950327db305d
Hay707 <hayleychan1430@gmail.com>
2023-11-03 12:43:52 -0700

    Did not complete, saving this to review problem

---
+++ world_traveller.py
@@ -0,0 +1,34 @@
+# World Traveller
+# Author: Hayley 
+# November 3 2023
+
+# Create a World Traveller 
+# It asks for the user's name and greets them. 
+# Then it asks if the user has been to each of the continents
+# After it finishes asking, it tells the user how many continents they've been to.
+# Must you loops
+
+# Ask for user's name and greet them
+name = input("What's your name?" + "\n")
+print(f"Hello {name}! It's nice to meet you")
+
+# Ask user about what continents they've been to. 
+questions = [
+    "Have you been to Asia?",
+    "Have you been to Europe?",
+    "Have you been to North America?",
+    "Have you been to South America",
+    "Have you been to Australia?", 
+    "Have you been to Anarctica?"
+]
+
+total_continents = 0
+ 
+ # Get users response to see how many continents they've been to 
+for continents in questions: 
+    if continents == "yes": 
+        total_continents += 1     
+    else: total_continents += 0    # problem heren 
+
+# Print out user result for how many continents they've been to
+print(f"I see, you've visited {total_continents}/7 continents!")
