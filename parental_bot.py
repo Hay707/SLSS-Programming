b31c475382bc104da424efa0e157cbd07a9a1b02
Hay707 <hayleychan1430@gmail.com>
2023-11-17 11:01:22 -0800

    Added explanation to the code

--- parental_bot.py
+++ parental_bot.py
@@ -3,7 +3,6 @@
 # November 17 2023
 
 # Ask questions 
-
 questions = [
     "Did you eat?",
     "Did you study?",
@@ -13,15 +12,19 @@ questions = [
 
 total_response = 0 
 
+# If answer = yes, add a score of 1 to total score
 for question in questions: 
     answer = (input(question).lower().strip(",.!?"))
 
     if answer.lower().strip(",.?!") == "yes":
         total_response += 1
 
-if total_response == 0: 
-    print("I'm coming over.")
+# If total response is 0, respond "I'm coming over"
+if total_response <= 4: 
+    print("Good!")
+# If total response is 1 or 2, respond "Ok."
 elif total_response <= 2:
     print("Ok.")
+# If total response is 3-4, respond "Good!"
 else:
-    print("Good!")
+    print("I'm coming over.")
