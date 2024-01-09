918bcd2c9dd5a3d154a91118b23b1aec4fbc63b4
Hay707 <hayleychan1430@gmail.com>
2024-01-09 12:25:37 -0800

    added to colour helper

--- Notes/colour_helper.py
+++ Notes/colour_helper.py
@@ -140,3 +140,28 @@ def picture_to_grayscale(filename: str) -> None:
         
         # Save the image
         im.save("./Images/grayscale.jpg")
+
+
+def pixel_to_name(pixel: tuple) -> str:
+    """Given a 3-tuple, return a string representing
+    its colour
+
+    Params:
+        pixel = 3-tuple of values (red, green, blue)
+
+    Returns:
+        name of the colour
+    """
+    red, green, blue = pixel
+
+    # TODO: detect redpixels
+    # red pixels -- r > 170, g> 60, b > 60
+    if red < 200 and blue < 200 and green > 220:
+        return "green"
+    elif red > 170 and green < 60 and blue < 60: 
+        return "red"
+    else:
+        return "colour unknown"
+    
+print(pixel_to_name((180, 3, 2)))
+print(pixel_to_name((255, 255, 255)))
